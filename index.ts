03b1b85b17086dcfee5e08ef01b962685e4a8b83
aryangupta1 <aryangupta1@outlook.com>
2021-08-11 14:27:03 +1000

    Middleware setup: Installation of cors

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import pool from './db';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
+
+//Middleware
+app.use(cors());
+//Allows us to access request.body to access JSON data
+app.use(express.json());
+
 app.get("/", (req, res) => res.send("HELLO THERE"));
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
