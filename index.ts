13270ef1aaf8684f19bbcad7cebf04c58a059494
aryangupta1 <aryangupta1@outlook.com>
2021-08-11 14:34:00 +1000

    Added create student api route

--- index.ts
+++ index.ts
@@ -14,3 +14,22 @@ app.get("/", (req, res) => res.send("HELLO THERE"));
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+//Routes
+
+/* Create new student - Very basic only using currentGroupStatus - which will be set to false by default. 
+This route is ideal for when a student signs up */
+app.post("/students", async(req, res) => {
+  try {
+    //Set to false by default
+    const currentGroupStatus = false;
+    const newStudent = await pool.query(
+      "INSERT INTO students (currentGroupStatus) VALUES($1) RETURNING *",
+      [currentGroupStatus]
+    );
+    res.json(newStudent.rows[0]);
+  } catch (error) {
+    console.error("An error occured: ", error.message);
+  }
+})
+//Students will add their preferences and skills etc in other routes below (TBD)
