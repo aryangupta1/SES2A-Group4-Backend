775698e4a529c17291a7f6220171ee3fd10e2041
aryangupta1 <aryangupta1@outlook.com>
2021-08-20 12:10:34 +1000

    U101.1 Generate jwt token

--- routes/jwtAuth.ts
+++ routes/jwtAuth.ts
@@ -3,6 +3,7 @@ import { getRepository } from "typeorm";
 import { Student } from "../entities/student.entity";
 import bcrypt from "bcrypt";
 const router = express.Router();
+import jwtGenerator from "../utils/jwtGenerator";
 
 
 //Register Route
@@ -24,7 +25,9 @@ router.post("/register", async (req, res) => {
         const newStudent = {firstName, lastName, email, password:bcryptPassword};
         const student = await studentRepository.create(newStudent);
         const results = await studentRepository.save(student);
-        res.json(results);
+        //Generate jwt token
+        const token = jwtGenerator(JSON.stringify(results.id));
+        res.json({token});
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Server Error");
