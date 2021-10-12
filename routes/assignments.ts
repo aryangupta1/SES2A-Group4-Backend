72a898fb8b364317de3b3351f191ab16a14db179
aryangupta1 <aryangupta1@outlook.com>
2021-10-13 09:18:34 +1100

    New endpoints for testing & frontend

--- routes/assignments.ts
+++ routes/assignments.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { appendFile } from "fs";
 const router = express.Router();
-import { getConnection, getRepository } from "typeorm";
+import { Admin, getConnection, getRepository } from "typeorm";
+import { admin } from "../entities/admin.entity";
 import { Assignment } from "../entities/assignment.entity";
 import { Group } from "../entities/group.entity";
 const connection = getConnection();
@@ -28,6 +29,45 @@ router.get("", async function (request, response){
     return response.send(allAssignments)
   }
   catch{
+    response.status(500).send("Server Error");
+  }
+});
+
+//Get students in group
+router.get("/:groupId/:assignmentName/students", async function(request, response){
+  try {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    const groupId = request.params.groupId;
+    const assignmentName = request.params.assignmentName;
+    console.log(groupId);
+    //find group
+    const groupRepository = await connection.getRepository(Group);
+    const group = await groupRepository.find({where: {assignmentName: assignmentName, id:groupId}});
+    //get students
+    const students = await group.map((item)=> item.students.map((student)=>student.email));
+    return response.send(students);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+})
+
+//get assignment through admin id and assignment name
+router.get("/:email/:assignmentName", async function (request, response) {
+  try{
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    const adminRepository = await connection.getRepository(admin);
+    const currentAdmin = await adminRepository.find({ where: { email: request.params.email } });
+    const adminId = await currentAdmin.map((i) => i.adminId);
+    const assignmentRepository = await connection.getRepository(Assignment);
+    const assignment = await assignmentRepository.find({
+      where: { admin: adminId.toString(), assignmentName: request.params.assignmentName },
+    });
+  
+    return response.send(assignment)
+  }
+  catch (error) {
     response.status(500).send("Server Error");
   }
 })
